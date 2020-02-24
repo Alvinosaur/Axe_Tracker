3cee926585a826e86a4967514771b323aaf982c2
Alvinosaur <ashek@andrew.cmu.edu>
2020-02-24 17:06:53 -0500

    finished generating all diff images

--- scripts_rpi/find_diff_images.py
+++ scripts_rpi/find_diff_images.py
@@ -3,8 +3,10 @@ import numpy as np
 import cv2
 import argparse
 import yaml
+import datetime
 
 from process_image import count_diff_SSIM
+import re
 
 
 # format = "2019-11-04"
@@ -23,41 +25,66 @@ positives_dir = "../../../axe_images/positives"
 negatives_dir = "../../../axe_images/negative_compare"
 diff_img_dir = "../../../axe_images/differences"
 YAML_FILEPATH = "default_params.yaml"
+file_format = "%Y-%m-%d_%H:%M:%S"
 
-with open(YAML_FILEPATH, 'r') as f:
-    params = yaml.load(f)
+def get_date(file):
+    date_match = re.search('\d{4}-\d{2}-\d{2}_\d{2}:\d{2}:\d{2}', file)
+    date = datetime.datetime.strptime(date_match.group(), file_format)
+    return date
 
-similarity_threshold = params["similarity_threshold"]
-count_threshold = (params["min_diff_pix"], params["max_diff_pix"])
-# store all negative filenames in mapping from date to the filename
-all_negatives = dict()
-for neg_file in os.listdir(negatives_dir):
-    if neg_file == ".DS_Store": continue
-    date = neg_file[8:10]
-    all_negatives[date] = os.path.join(negatives_dir, neg_file)
+def find_best_neg(negatives_map, negatives_list, pos_file):
+    # '2019-10-31_23:59:59_diff:2563.png'
+    target = get_date(pos_file)
+    best_date = min(negatives_list, key=lambda date : abs(target-date))
+    return negatives_map[best_date]
 
-# loop through all positives, generate diff image, store
-wrong_diff = []
-for pos_file in os.listdir(positives_dir):
-    if pos_file == ".DS_Store": continue
-    pos_path = os.path.join(positives_dir, pos_file) 
-    pos = cv2.cvtColor(cv2.imread(pos_path), cv2.COLOR_BGR2RGB)
+def main():
+    # load tuned parameters
+    with open(YAML_FILEPATH, 'r') as f:
+        params = yaml.load(f)
+    similarity_threshold = params["similarity_threshold"]
+    count_threshold = (params["min_diff_pix"], params["max_diff_pix"])
 
-    date = pos_file[8:10]
-    neg_file = all_negatives[date]
-    neg = cv2.cvtColor(cv2.imread(neg_file), cv2.COLOR_BGR2RGB)
+    # store all negative filenames in mapping from date to the filename
+    neg_date_to_file = dict()  # datetime -> datetime-specific filepath
+    neg_dates = []  # all datetimes
+    for neg_file in os.listdir(negatives_dir):
+        if neg_file == ".DS_Store": continue
+        date = get_date(neg_file)
+        neg_dates.append(date)
+        neg_date_to_file[date] = os.path.join(negatives_dir, neg_file)
 
-    diff, diff_img, diff_img_annotated = count_diff_SSIM(pos, neg,
-        similarity_threshold)
+    # loop through all positives, generate diff image, store
+    wrong_diff = []
+    all_pos = set(os.listdir(positives_dir))
+    seen_pos = set(os.listdir(diff_img_dir))
+    leftover = list(all_pos.difference(seen_pos))
 
-    cv2.imshow("pos, neg", np.hstack((pos, neg)))
-    cv2.imshow("diff", diff_img_annotated)
-    key = cv2.waitKey(0) & 0xFF
-    if key == ord('n'):
-        wrong_diff.append(pos_path)
-    elif key == ord('y'):
-        new_path = os.path.join(diff_img_dir, pos_file)
-        cv2.imwrite(new_path, diff_img)
-    else:
-        print("Wrong Key! Added to missed list")
-        wrong_diff.append(pos_path)
+    for pos_file in leftover:
+        if pos_file == ".DS_Store": continue
+        pos_path = os.path.join(positives_dir, pos_file) 
+        pos = cv2.cvtColor(cv2.imread(pos_path), cv2.COLOR_BGR2RGB)
+
+        neg_path = find_best_neg(neg_date_to_file, neg_dates, pos_file)
+        print([pos_file, neg_path])
+
+        neg = cv2.cvtColor(cv2.imread(neg_path), cv2.COLOR_BGR2RGB) 
+        diff, diff_img, diff_img_annotated = count_diff_SSIM(pos, neg,
+            similarity_threshold)
+
+        cv2.imshow("pos, neg", np.hstack((pos, neg)))
+        cv2.imshow("diff", diff_img_annotated)
+        key = cv2.waitKey(0) & 0xFF
+        if key == ord('n'):
+            wrong_diff.append(pos_path)
+        elif key == ord('y'):
+            new_path = os.path.join(diff_img_dir, pos_file)
+            cv2.imwrite(new_path, diff_img)
+        else:
+            print("Wrong Key! Added to missed list")
+            wrong_diff.append(pos_path)
+    
+    print("Files that were rejected:")
+    print(wrong_diff)
+
+main()
