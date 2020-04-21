6fb6b93a27a1db618cb12b3e06ffc51cc6f2d110
Alvinosaur <ashek@andrew.cmu.edu>
2020-04-21 10:11:23 -0700

    functional import of maskrcnn and use of pretrained weights

--- scripts_rpi/maskrcnn_scripts/run_maskrcnn.py
+++ scripts_rpi/maskrcnn_scripts/run_maskrcnn.py
@@ -23,17 +23,6 @@ from mrcnn import visualize
 
 WEIGHTS_PATH = "mask_rcnn_final_weights.h5"
 
-def get_ax(rows=1, cols=1, size=8):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-    
-    Change the default size attribute to control the size
-    of rendered images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
-    return ax
-
 # Training dataset
 dataset_train = AxeDataset.AxeDataset()
 dataset_train.load_axe_images(AxeDataset.TRAIN_DIR)
@@ -58,33 +47,33 @@ inference_config = InferenceConfig(2)
 # Recreate the model in inference mode
 model = modellib.MaskRCNN(mode="inference", 
                           config=inference_config,
-                          model_dir="")
+                          model_dir=ROOT_DIR + "/logs")
 model_path = WEIGHTS_PATH
+model.load_weights(model_path, by_name=True)
 
-# Load some test image
-image_id = random.choice(dataset_test.image_ids)
-original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-    modellib.load_image_gt(dataset_val, inference_config, 
-                           image_id, use_mini_mask=False)
-visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
-                            dataset_train.class_names, figsize=(8, 8))
+for i in range(10):
+    # Load some test image
+    image_id = random.choice(dataset_test.image_ids)
+    original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
+        modellib.load_image_gt(dataset_val, inference_config, 
+                            image_id, use_mini_mask=False)
+    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
+                                dataset_train.class_names, figsize=(8, 8))
 
-# run image through network to predict axe location
-results = model.detect([original_image], verbose=1)
-print("Displaying predicted:")
-r = results[0]
-show_all_boxes = True
-if show_all_boxes:
-    visualize.display_instances(original_image, r['rois'], r['masks'], 
-        r['class_ids'], dataset_val.class_names, r['scores'], figsize=(8, 8))
-else:
-    best_i = np.argmax(r['scores'])
-    best_roi = np.reshape(r['rois'][best_i], newshape=(1,4))
-    h, w, _ = r['masks'].shape
-    best_mask = np.reshape(r['masks'][:,:,best_i], newshape=(h,w,1))
-    best_class_id = np.array([r['class_ids'][best_i]])
-    best_score = np.array([r['scores'][best_i]])
-    visualize.display_instances(original_image, best_roi, best_mask, 
-        best_class_id, dataset_val.class_names, best_score, figsize=(8, 8))
-    # visualize.display_instances(original_image, best_roi, best_mask, 
-    #     best_class_id, dataset_val.class_names, best_score, ax=get_ax())
+    # run image through network to predict axe location
+    results = model.detect([original_image], verbose=1)
+    print("Displaying predicted:")
+    r = results[0]
+    show_all_boxes = False
+    if show_all_boxes:
+        visualize.display_instances(original_image, r['rois'], r['masks'], 
+            r['class_ids'], dataset_val.class_names, r['scores'], figsize=(8, 8))
+    else:
+        best_i = np.argmax(r['scores'])
+        best_roi = np.reshape(r['rois'][best_i], newshape=(1,4))
+        h, w, _ = r['masks'].shape
+        best_mask = np.reshape(r['masks'][:,:,best_i], newshape=(h,w,1))
+        best_class_id = np.array([r['class_ids'][best_i]])
+        best_score = np.array([r['scores'][best_i]])
+        visualize.display_instances(original_image, best_roi, best_mask, 
+            best_class_id, dataset_val.class_names, best_score, figsize=(8, 8))
